630eae8061430d1db7d189c58b91a2df2f0c7013
Laura Escobar Rojo <l.escobar@globant.com>
2025-09-03 14:18:27 -0500

    first trial

--- src/test/java/com/automation/pom/pages/LoginPage.java
+++ src/test/java/com/automation/pom/pages/LoginPage.java
@@ -1,4 +1,54 @@
 package com.automation.pom.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    private WebDriver driver;
+    public static final String LOGIN_URL = "https://www.saucedemo.com/";
+
+    // ====== WebElements ======
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = "h3[data-test='error']")
+    private WebElement errorMessage;
+
+    // ====== Constructor ======
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // ====== Actions ======
+    public void enterUsername(String username) {
+        usernameField.clear();
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        return errorMessage.isDisplayed();
+    }
 }
--- src/test/java/com/automation/pom/tests/LoginTest.java
+++ src/test/java/com/automation/pom/tests/LoginTest.java
@@ -1,4 +1,57 @@
 package com.automation.pom.tests;
 
+import com.automation.pom.pages.LoginPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(LoginPage.LOGIN_URL);
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void successfulLoginTest() {
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
+
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("inventory.html"),
+                "User should be redirected to inventory page after login");
+    }
+
+    @Test
+    public void failedLoginTest() {
+        loginPage.enterUsername("locked_out_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
+
+        Assert.assertTrue(loginPage.isErrorMessageDisplayed(),
+                "Error message should be displayed for locked_out_user");
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Sorry, this user has been locked out.");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
